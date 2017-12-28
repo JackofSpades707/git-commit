2a3cd88d83a8721b8142b1695ae9020ec8dea96d
jackofspades707 <jackofspades707@hotmail.com>
2017-12-27 22:17:57 -0800

    [!] setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from sys import platform
 
 setup(name='git_commit',
-        version='1.01d',
+        version='1.02',
         description='Easily Standardize your github commit automatically!',
         url='https://github.com/jackofspades707/git-commit',
         author='JackofSpades707',
